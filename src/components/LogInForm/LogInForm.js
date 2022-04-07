333171e8b94a8f855af3857766691e719326ec0c
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-07 19:37:17 +0600

    add firebase sign in method in login form

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -1,22 +1,58 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../../firebase.init.';
 
+const auth = getAuth(app);
 const LogInForm = () => {
+  // State
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className='w-50 mx-auto mt-5 border border-1 border-info p-5 rounded-3'>
-      <Form>
+      <Form onSubmit={handleLogin}>
         <h2>Please Login</h2>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type='email'
+            placeholder='Enter email'
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            type='password'
+            placeholder='Password'
+          />
         </Form.Group>
+        <Form.Text className='text-danger'>{error}</Form.Text>
         <div className='mb-3'>
           <Link to={'/register'}>Don't have an account?</Link>
         </div>
